9cc0e59d4d0f63a1dc978733b4adbbcb4b8b67fc
praifaeiei <645051200089@mail.rmutk.ac.th>
2024-01-17 13:18:05 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:testt/gradient_container.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home : Scaffold(
+      body : GContainer(),
+    )));
+}
+
---
+++ lib/stylet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:testt/gradient_container.dart';
+
+class StyleT extends StatelessWidget{
+  StyleT ({super.key});
+  
+   Widget build(context){
+    return Container(
+      
+        child: Text('ʕ•ᴥ•ʔ 𝑻𝒉𝒂𝒏𝒚𝒂𝒑𝒐𝒓𝒏 ʕ•ᴥ•ʔ',style: TextStyle(fontSize: 30, 
+        color: Color.fromARGB(255, 219, 123, 147))),
+    );
+  }
+
+}
