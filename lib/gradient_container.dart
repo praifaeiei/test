007252a777f89097c7dcf4e292b06b27cc393901
praifaeiei <645051200089@mail.rmutk.ac.th>
2024-01-24 11:42:13 +0700

    testt

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:testt/stylet.dart';
+import 'package:testt/dic.dart';
 
 class GContainer extends StatelessWidget{
   GContainer({super.key});
 
-  Widget build(context){
+
+  Widget build (context) {
     return Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 223, 171, 210),
+        gradient: LinearGradient(colors: [
+           Color.fromARGB(255, 223, 171, 210),
             Color.fromARGB(224, 188, 166, 223),
             Color.fromARGB(223, 202, 228, 154),
             Color.fromARGB(223, 161, 208, 223),
-          ],
-          begin: Alignment.topRight,end: Alignment.bottomLeft
-        )
-        ),
-        child: Center(
-        child: StyleT()
-        // style: TextStyle(fontSize: 30, color: Color.fromARGB(255, 219, 123, 147))),
-    ));
+
+
+        ],begin:Alignment.topRight,end:Alignment.bottomLeft)),
+      child:  Center(child: DiceThrown()));
   }
 }
